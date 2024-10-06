c5c50fbbdef29389cf15c2e8921db4d31d207a95
ahuallpara <104446232+ahuallpara@users.noreply.github.com>
2024-10-06 13:50:00 -0400

    Update plot.py
    works for newer python version

--- dispaset/postprocessing/plot.py
+++ dispaset/postprocessing/plot.py
@@ -292,7 +292,7 @@ def plot_rug(df_series, on_off=False, cmap='Greys', fig_title='', normalized=Fal
                             figsize=(16, 0.25 * rows), squeeze=False,
                             frameon=True, gridspec_kw={'hspace': 0.15})
 
-    for (item, iseries), iax in zip(df_series.iteritems(), axes.ravel()):
+    for (item, iseries), iax in zip(df_series.items(), axes.ravel()):
         format_axis(iax)
         iax.set_ylabel(str(item)[:30], rotation='horizontal',
                        rotation_mode='anchor',
